d063d863e2a86931ec87c2efcd125a2c3319ac39
cgbridgewater <cgbridgewater@outlook.com>
2023-02-05 21:57:16 -0800

    first commit!

---
+++ Java Fundamentals/Greetings/MyOwnJava.java
@@ -0,0 +1,6 @@
+public class MyOwnJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Chris B.\nI am 43 years old\nMy hometown is Creswell, OR"
+        );
+    }
+}
