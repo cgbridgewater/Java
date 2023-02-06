4f424733b7f7000aa6d53e99f5e585b8f49e5d40
cgbridgewater <cgbridgewater@outlook.com>
2023-02-06 14:58:55 -0800

    adding switch operation tests

--- Java Fundamentals/Greetings/MyOwnJava.java
+++ Java Fundamentals/Greetings/MyOwnJava.java
@@ -2,5 +2,33 @@ public class MyOwnJava {
     public static void main(String[] args) {
         System.out.println("My name is Chris B.\nI am 43 years old\nMy hometown is Creswell, OR\nI'm testing my GH Repo Change with this line."
         );
+
+        System.out.println("Switch Operator says....");
+        int day = 2;
+switch(day){
+	case 1:
+		System.out.println("Monday");
+		break;
+	case 2:
+		System.out.println("Tuesday");
+		break;
+	case 3:
+		System.out.println("Wednesday");
+		break;
+	case 4:
+		System.out.println("Thursday");
+		break;
+	case 5:
+		System.out.println("Friday");
+		break;
+	case 6:
+		System.out.println("Saturday");
+		break;
+	case 7:
+		System.out.println("Sunday");
+		break;
+	default:
+		System.out.println("There are only seven days in a week...");
+}
     }
 }
