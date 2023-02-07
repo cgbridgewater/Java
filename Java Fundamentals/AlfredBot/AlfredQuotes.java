8a93881024cb90ff8ca63f553a6b5850863200a4
cgbridgewater <cgbridgewater@outlook.com>
2023-02-07 13:58:16 -0800

    cleanup

--- Java Fundamentals/AlfredBot/AlfredQuotes.java
+++ Java Fundamentals/AlfredBot/AlfredQuotes.java
@@ -1,31 +1,26 @@
 import java.util.Date;
 public class AlfredQuotes {
     
+    // basic greeting
     public String basicGreeting() {
-        // You do not need to code here, this is an example method
         return "Welcome to the BatCave!  Please don't touch the Batmobile.";
     }
 
-
-                                            //,int dayPeriod
+    // guest greeting with name in function
     public String guestGreeting(String name) {
-        // if(name){
             return "Welcome to the Wayne Mannor " + name;
-        }
-
-    public String guestGreeting2(String name, String dayPeriod) {
-        return String.format("Good %s, %s. Welcome to the Batcave", dayPeriod, name);
     }
 
-
+    // using date time to announce time of day
     public String dateAnnouncement() {
         Date date = new Date();
         return "The Official Bat Time is " + date;
     }
     
+    //using an IF statement to utilize indexOf to inspect string for specific words present and respond
     public String respondBeforeAlexis(String conversation) {
         if(conversation.indexOf("Alexis")>=0) {
-             return "Don't ask her, she doesn't know anything if she can't google what it is";
+            return "Don't ask her, she doesn't know anything if she can't google what it is";
         }
         else if(conversation.indexOf("Alfred")>0) {
             return "Right away sir, I will find that for you...";
@@ -33,14 +28,14 @@ public class AlfredQuotes {
         else {
             return "That's good stuff sir, let me know if I can help with anything, like unplugging your FBI, I mean 'Alexa' device.";
         }
-
-        // return "Don't ask her, she doesn't know anything.";
     }
     
-	// NINJA BONUS
-	// See the specs to overload the guessGreeting method
-    // SENSEI BONUS
-    // Write your own AlfredQuote method using any of the String methods you have learned!
+	// NINJA BONUS method overload
+    public String guestGreeting2(String name, String dayPeriod) {
+        return String.format("Good %s, %s. Welcome to the Batcave", dayPeriod, name);
+    }
+
+    // SENSEI BONUS making my own method
     public String batmobileTouched(String name) {
         return name + ", I wasn't joking when I told you not to touch the Batmobile! Who do you think I am, The Joker?";
     }
