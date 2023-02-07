818c71dcb0d5fff814b158c298973e7ce1cd7531
cgbridgewater <cgbridgewater@outlook.com>
2023-02-06 17:27:14 -0800

    clean up and finish

--- Java Fundamentals/Greetings/CafeJava.java
+++ Java Fundamentals/Greetings/CafeJava.java
@@ -29,15 +29,15 @@ public class CafeJava {
         boolean isReadyOrder3 = false;
         boolean isReadyOrder4 = true;
     
+
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
-        // Example:
-        //System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
-    	// ** Your customer interaction print statements will go here ** //
+        // #1
         System.out.println("customer scenario 1");
         System.out.println(generalGreeting + customer1);
         System.out.println(displayTotalMessage + dripCoffee + (isReadyOrder1 ? readyMessage:pendingMessage));
 
 
+        // #2
         System.out.println("customer scenario 2");
         if(isReadyOrder4) {
         System.out.println(customer4 + readyMessage + ". " + displayTotalMessage + cappucino);
@@ -47,7 +47,7 @@ public class CafeJava {
         }
 
 
-
+        // #3
         System.out.println("customer scenario 3");
         if(isReadyOrder2) {
         System.out.println(customer2 + readyMessage + ". " + displayTotalMessage + (cappucino*2));
@@ -56,10 +56,7 @@ public class CafeJava {
             System.out.println(customer2 + pendingMessage);
         }
 
-
-        // error = Math.round((latte-dripCoffee)*100)/100;
-        // (Math.round((latte-dripCoffee)*100)/100)
-
+        // #4
         System.out.println("customer scenario 4");
         double roundError = Math.round((latte-dripCoffee)*100)/100.0;
         System.out.println(customer3 + " " + displayTotalMessage + roundError + (isReadyOrder3 ? readyMessage:pendingMessage) );
