60fa6de1cccd90e04796efc692b459c4b6dbb1a2
cgbridgewater <cgbridgewater@outlook.com>
2023-02-07 20:42:25 -0800

    update

--- Java Fundamentals/Loops/Loops.java
+++ Java Fundamentals/Loops/Loops.java
@@ -84,6 +84,7 @@ class Loops {
         int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         for(int i =1; i <10; i +=2){
             System.out.println(i);
+            // System.out.println(numbers);
         }
     }
 }
