8685790c7266fd2fa45477aeebd4f9cd43a6cc9d
cgbridgewater <cgbridgewater@outlook.com>
2023-02-09 15:03:00 -0800

    completed 4 methods plus int input

--- Java Fundamentals/Puzzling/PuzzleJava.java
+++ Java Fundamentals/Puzzling/PuzzleJava.java
@@ -18,7 +18,7 @@ public void getRandomLetter(ArrayList<String> randomLetter){
     String[] alphabetSoup = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
     Random randomInt = new Random(); // declares random function
     int random = randomInt.nextInt(26); // makes a random number
-    System.out.printf("Let me pull a randome letter out of thin air for you. Drumroll please.....  Here's your letter: %s", alphabetSoup[random]);
+    System.out.printf("Let me pull a random letter out of thin air for you. Drumroll please.....  Here's your letter: %s", alphabetSoup[random]);
     System.out.printf("\n"); 
 }
 
@@ -26,21 +26,41 @@ public void getRandomLetter(ArrayList<String> randomLetter){
 public void generatePassword(String randomPW){
     String[] letterOptions = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
     Random letterPicker = new Random(); // declares random function
-        for (int i = 0; i < 8; i++) { // Creates 10 slots in the array
+        for (int i = 0; i < 8; i++) { // Creates 8 slots in the string
         int randomLocation = letterPicker.nextInt(26); // makes a random number 
-        // randomPW.add(letterOptions[randomLocation]);
-        randomPW += letterOptions[randomLocation];
+        randomPW += letterOptions[randomLocation];//concatinates the new letter with the old ones
         }
     System.out.printf("Your random password is : %s ", randomPW); 
     System.out.printf("\n"); 
 }
-// generatePassword
-// white a method that uses the previous methode to create a random string of 8 characters and return the string
+
+
+public void getNewPasswordSet(String randomPassword, int pwQty){
+    String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    Random letterSelector = new Random(); // declares random function
+    ArrayList<String> passwordArray = new ArrayList<String>();
+    int count = 0;
+        while (count < pwQty) {
+        // for (int i = 0; i < pwQty; i++) {
+            for (int ii = 0; ii < 8; ii++) { // Creates 8 slots in the string
+                int randomLocation = letterSelector.nextInt(26); // makes a random number 
+                randomPassword += alphabet[randomLocation]; //concatinates the new letter with the old ones
+                }
+            // System.out.printf("Your random password is : %s ", randomPassword); // print each password before adding to string for trouble shooting
+            // System.out.printf("\n"); 
+            passwordArray.add(randomPassword);
+            randomPassword = "";
+            count++;
+        }
+
+    System.out.printf("\nYour list of %s passwords is : %s ",pwQty, passwordArray); 
+    System.out.printf("\n"); 
+}
 
 
 
 // getNewPasswordSet 
-// write a method that takes an "int" length as an argument and creates an array of 8 charactor words. 
+// write a method that takes an "int" length as an argument and creates an array of 8 charactor passwords. 
 // The array should be the length of the "int" provided
 // return the arraw of the passwords
 
--- Java Fundamentals/Puzzling/TestPuzzleJava.java
+++ Java Fundamentals/Puzzling/TestPuzzleJava.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class TestPuzzleJava {
@@ -10,29 +11,33 @@ public class TestPuzzleJava {
         
         PuzzleJava generator = new PuzzleJava();
 
-
-        System.out.println("\n----- Generate an array of Random Rolla-----");
+        //#1
+        System.out.println("\n----- Generate an array of Random Rolls-----");
         ArrayList<Integer> randomRolls = new ArrayList<Integer>();
             generator.getTenRolls(randomRolls);
         
         
-        
+        //#2
         System.out.println("\n----- Generate a single random letter-----");
         ArrayList<String> randomLetter = new ArrayList<String>();
             generator.getRandomLetter(randomLetter);
 
         
+        //#3
         System.out.println("\n----- Generate a random 8 letter password-----");
         String randomPW ="";
             generator.generatePassword(randomPW);
 
 
 
-
-        // System.out.println("----- Generate a random passwords basedo on int input-----");
-        // ArrayList<String> randomLetter = new ArrayList<String>();
-            // int pwQty;
-        //     generator.getNewPasswordSet(randomLetter, pwQty);
+        //#4
+        System.out.println("\n----- Generate a random passwords based on 'int' input-----");
+        String randomPassword = "";
+        System.out.println("\n Please enter the number of random passwords you would like created: \n");
+        // int pwQty = 5;
+        Scanner in = new Scanner(System.in);
+        int pwQty = in.nextInt();
+            generator.getNewPasswordSet(randomPassword, pwQty);
 
 
 
