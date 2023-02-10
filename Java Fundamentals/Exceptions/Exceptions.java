2d8695016294b5232fdef3a17db0de0b5110047e
cgbridgewater <cgbridgewater@outlook.com>
2023-02-09 21:41:17 -0800

    First Commit

---
+++ Java Fundamentals/Exceptions/Exceptions.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Exceptions {
+    public static void main(String[] args) {
+
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+
+
+        for(int i = 0; i < myList.size(); i++) {
+        try{
+            Integer castedValue = (Integer) myList.get(i);
+            System.out.println("Typecast Successful for Object Location: " + i);
+        }
+        catch (Exception e){
+            System.out.println("Typecast Error for Object Location: " + i);    
+        }   
+        }
+    }
+}
