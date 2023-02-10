79f0c566705840cdeef463850731f387a8b5e991
cgbridgewater <cgbridgewater@outlook.com>
2023-02-09 19:10:43 -0800

    first commit

---
+++ Java OOP/classwk1d3/LibraryTest.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class LibraryTest {
+
+    public static void main(String[] args){
+
+        Book theHobbit = new Book ("The Hobbit", "J.R.R Tolkien", "Fantasy",  295);
+        Book theLordOfTheRings = new Book ("The Lord of the Rings", "J.R.R Tolkien", "Fantasy",  1200);
+        Book theSilmarillion = new Book ("The The Silmarillion", "J.R.R Tolkien", "Fantasy",  700);
+
+
+    // Member jason = new Member();
+    // jason.setFirstName(firstName:"Jason")
+
+    Member chris = new Member( firstName: "Chris", lastName: "Bridgewater", memberNumber: 1);
+
+
+    chris.checkOutBook(theSilmarillion);
+    chris.checkOutBook(theSilmarillion);
+    System.out.println(chris.getBooksCheckedOut());
+    chris.returnBook(theSilmarillion);
+    System.out.println(chris.getBooksCheckedOut());
+
+
+
+    }
+}
---
+++ Java OOP/classwk1d3/Member.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+
+
+public class Member {
+
+    private String firstName;
+    private String lastName;
+    private Integer memberNumber;
+    private ArrayList<Book> booksCheckedOut;
+
+
+    public Member( String firstName, String lastName, Integer memberNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.memberNumber = memberNumber;
+        this.booksCheckedOut = new ArrayList<Book>();
+    }
+
+    public Member(){
+        this.booksCheckedOut = new ArrayList<Book>();
+    }; //default constructor
+
+    public void checkOutBook(Book book){
+        booksCheckedOut.add(book);
+    }
+
+    public void returnBook(Book book){   // should be (int index)
+        booksCheckedOut.remove(book);    // remove(index)
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String setFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public String setLastName() {
+        return lastName;
+    }
+    
+    public Integer getMemberNumber() {
+        return memberNumber;
+    }
+    public Integer setMemberNumber() {
+        return memberNumber;
+    }
+
+    public ArrayList<Book> getBooksCheckedOut() {
+        return booksCheckedOut;
+    }
+
+    public ArrayList<Book> setBooksCheckedOut() {
+        return booksCheckedOut;
+    }
+
+
+
+
+
+
+}
