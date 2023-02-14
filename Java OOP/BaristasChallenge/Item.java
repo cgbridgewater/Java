2c0915c9ca768af6d7485b4885a5bc8ec73a4ac7
cgbridgewater <cgbridgewater@outlook.com>
2023-02-13 17:18:11 -0800

    updating per lesson

--- Java OOP/BaristasChallenge/Item.java
+++ Java OOP/BaristasChallenge/Item.java
@@ -1,6 +1,33 @@
 class Item {
 
-    public String name;
-    public double price;
+
+//Item class with name and price, private so they can be controlled by only ME!
+    private String name;
+    private double price;
     
-} //Item class with name and price
+
+//constructor with name & price as arguments
+    public Item(String name, double price){
+        this.name= name;
+        this.price=price;
+    }
+
+    public void setName(String name) {  // name SETTER //
+        this.name = name;
+    }
+
+    public String getName() {  // name GETTER //
+        return name;
+    }
+
+    public void setPrice(double number) {  // price SETTER //
+        this.price = price;
+    }
+
+    public double getPrice() {  // price GETTER //
+        return price;
+    }
+
+
+
+} 
--- Java OOP/BaristasChallenge/Order.java
+++ Java OOP/BaristasChallenge/Order.java
@@ -2,9 +2,51 @@ import java.util.ArrayList;
 
 class Order {
 
-    public String name;
-    public double total;
-    public boolean ready;
-    public ArrayList<Item> drinks = new ArrayList<Item>();
+    private String name;
+    // private double total;
+    private boolean ready;
+    private ArrayList<Item> drinks = new ArrayList<Item>();
 
-} // Order class with Name, total, ready? and drink list array (from Item Class)
+
+
+//constructor with name & price as arguments
+    public Order(String name, boolean ready, ArrayList<Item> drinks){ //, double total
+        this.name = name;
+        // this.total = total;
+        this.ready = ready;
+        this.drinks = drinks; 
+    }
+
+    public void setName(String name) {  // name SETTER //
+        this.name = name;
+    }
+
+    public String getName() {  // name GETTER //
+        return name;
+    }
+
+    // public void setTotal(double total) {  // total SETTER //
+    //     this.total = total;
+    // }
+
+    // public double setTotal() {  // total GETTER //
+    //     return total;
+    // }
+
+    public void setReady(boolean ready) {  // ready SETTER //
+        this.ready = ready;
+    }
+
+    public double getPrice() {  // price GETTER //
+        return price;
+    }
+
+    public void setDrinks(ArrayList<Item> drinks) {  // drinks SETTER //
+        this.drinks = drinks;
+    }
+
+    public ArrayList<Item> getDrinks() {  // drinks GETTER //
+        return drinks;
+    }
+
+}
