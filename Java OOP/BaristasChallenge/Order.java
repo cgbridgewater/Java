af4ac99480a6457deb3ad621fab86acfb4e31e82
cgbridgewater <cgbridgewater@outlook.com>
2023-02-13 17:00:45 -0800

    first commit

---
+++ Java OOP/BaristasChallenge/Order.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+class Order {
+
+    public String name;
+    public double total;
+    public boolean ready;
+    public ArrayList<Item> drinks = new ArrayList<Item>();
+
+} // Order class with Name, total, ready? and drink list array (from Item Class)
