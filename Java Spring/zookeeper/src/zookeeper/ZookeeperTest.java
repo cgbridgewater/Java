a0d1fcf4fa2b43bfb7a0c617fbc8a7f6aeb47eba
cgbridgewater <cgbridgewater@outlook.com>
2023-02-16 14:51:11 -0800

    update and finish

--- Java Spring/zookeeper/src/zookeeper/ZookeeperTest.java
+++ Java Spring/zookeeper/src/zookeeper/ZookeeperTest.java
@@ -5,14 +5,14 @@ public class ZookeeperTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		Gorilla g1 = new Gorilla();
-//		System.out.println(g1.throwSomething());
-//		System.out.println(g1.climb());
-//		System.out.println(g1.eatBananas());
-//		System.out.println(g1.throwSomething());
-//		System.out.println(g1.eatBananas());
-//		System.out.println(g1.throwSomething());
-//		System.out.println(g1.displayEnergy());
+		Gorilla g1 = new Gorilla();
+		System.out.println(g1.throwSomething());
+		System.out.println(g1.climb());
+		System.out.println(g1.eatBananas());
+		System.out.println(g1.throwSomething());
+		System.out.println(g1.eatBananas());
+		System.out.println(g1.throwSomething());
+		System.out.println(g1.displayEnergy());
 
 		GiantFugginBat bat1 = new GiantFugginBat();
 		System.out.println(bat1.attackTown());
