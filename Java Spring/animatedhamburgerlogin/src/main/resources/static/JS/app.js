a69895b20742d6aa262ab6590d72066fec0a6472
cgbridgewater <cgbridgewater@outlook.com>
2023-03-14 18:38:48 -0700

    first commit

---
+++ Java Spring/animatedhamburgerlogin/src/main/resources/static/JS/app.js
@@ -0,0 +1,23 @@
+/**
+const signUpBtnLink = document.querySelector('.signUpBtn-link');
+
+ const signInBtnLink = document.querySelector('.signInBtn-link');
+
+ const wrapper = document.querySelector('.wrapper');
+ 
+ const regButton = document.querySelector('.button2');
+ 
+ 
+ 
+ signUpBtnLink.addEventListener('click',()=> {
+	 wrapper.classList.toggle('active');
+ });
+ 
+  signInBtnLink.addEventListener('click',()=> {
+	 wrapper.classList.toggle('active');
+ });
+
+  regButton.addEventListener('click',()=> {
+	 wrapper.classList.toggle('inactive');
+ });
+ */
---
+++ Java Spring/overflowdojo/src/main/java/com/chrisb/overflowdojo/OverflowdojoApplication.java
@@ -0,0 +1,13 @@
+package com.chrisb.overflowdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverflowdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverflowdojoApplication.class, args);
+	}
+
+}
